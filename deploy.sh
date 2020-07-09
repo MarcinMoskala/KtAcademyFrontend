dba17356e781e82c3beacad9f53250664f9c9848
Marcin Moskala <marcin.moskala@allegro.pl>
2020-07-09 19:50:12 +0200

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,23 @@
+#!/usr/bin/env sh
 
+# abort on errors
+set -e
+
+# build
+echo Linting..
+npm run lint
+
+echo Building. this may take a minute...
+npm run build
+
+echo Deploying..
+# navigate into the build output directory
+cd dist
 
 git init
-git remote add origin git@github.com:MarcinMoskala/KtAcademyFrontendDeploy.git
+git remote add origin git@github.com:MarcinMoskala/KtAcademyFrontendDeploy.git || true
 git add -A
 git commit -m 'new deploy'
 git push -u origin master -f
+
+cd -
